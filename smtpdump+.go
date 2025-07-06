b22c316ec24d3912b237cde50013cf873e0a91ce
Stefan Claas <sacenator@gmail.com>
2025-07-06 12:20:52 +0200

    Add files via upload

---
+++ smtpdump+.go
@@ -0,0 +1,317 @@
+package main
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/tls"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/mail"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/mhale/smtpd"
+	"golang.org/x/time/rate"
+)
+
+const (
+	ReadTimeout          = 60 * time.Second
+	WriteTimeout         = 60 * time.Second
+	InputLimit           = 1024 * 1024
+	MailQueueBufferSize  = 100
+	FileSaverWorkerCount = 5
+	RandomNameLength     = 8
+)
+
+var (
+	addr        = flag.String("addr", "127.0.0.1:2525", "Listen address:port")
+	cert        = flag.String("cert", "", "PEM-encoded certificate")
+	colorize    = flag.Bool("color", true, "colorize debug output")
+	discard     = flag.Bool("discard", false, "discard incoming messages")
+	extension   = flag.String("extension", "eml", "Saved file extension")
+	minTLS11    = flag.Bool("tls11", false, "accept TLSv1.1 as a minimum")
+	minTLS12    = flag.Bool("tls12", false, "accept TLSv1.2 as a minimum")
+	minTLS13    = flag.Bool("tls13", false, "accept TLSv1.3 as a minimum")
+	pkey        = flag.String("key", "", "PEM-encoded private key")
+	verbose     = flag.Bool("verbose", false, "verbose output")
+	readPrintf  = color.New(color.FgGreen).Printf
+	writePrintf = color.New(color.FgCyan).Printf
+	hostname    string
+
+	mailSaverQueue chan *mailDataToSave
+)
+
+type mailDataToSave struct {
+	RemoteAddr net.Addr
+	From       string
+	To         []string
+	Data       []byte
+	ReceivedAt time.Time
+	RetryCount int
+}
+
+func init() {
+	hn, err := os.Hostname()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	flag.StringVar(&hostname, "hostname", hn, "Server host name")
+	flag.BoolVar(&smtpd.Debug, "debug", false, "debug output")
+}
+
+func main() {
+	flag.Parse()
+	if hostname == "" {
+		log.Fatalln("Hostname cannot be empty")
+	}
+	if smtpd.Debug {
+		*verbose = true
+		if !*colorize {
+			readPrintf = fmt.Printf
+			writePrintf = fmt.Printf
+		}
+	}
+
+	syscall.Umask(0022)
+
+	mailSaverQueue = make(chan *mailDataToSave, MailQueueBufferSize)
+	startFileSaverWorkers(mailSaverQueue, FileSaverWorkerCount, *extension, *verbose)
+
+	var handler smtpd.Handler
+	if *discard {
+		handler = discardHandler(*verbose)
+	} else {
+		handler = queuingOutputHandler(mailSaverQueue, *verbose)
+	}
+
+	srv := &smtpd.Server{
+		Addr:        *addr,
+		Appname:     "SMTPDump",
+		AuthHandler: authHandler,
+		Handler:     handler,
+		MaxSize:     InputLimit,
+		LogRead: func(_, _, line string) {
+			line = strings.Replace(line, "\n", "\n  ", -1)
+			_, _ = readPrintf("  %s\n", line)
+		},
+		LogWrite: func(_, _, line string) {
+			line = strings.Replace(line, "\n", "\n  ", -1)
+			_, _ = writePrintf("  %s\n", line)
+		},
+		HandlerRcpt: rcptHandler,
+	}
+
+	if *cert != "" && *pkey != "" {
+		err := srv.ConfigureTLS(*cert, *pkey)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("Enabled TLS support")
+		switch {
+		case *minTLS13:
+			srv.TLSConfig.MinVersion = tls.VersionTLS13
+			log.Println("Minimum TLSv1.3 accepted")
+		case *minTLS12:
+			srv.TLSConfig.MinVersion = tls.VersionTLS12
+			log.Println("Minimum TLSv1.2 accepted")
+		case *minTLS11:
+			srv.TLSConfig.MinVersion = tls.VersionTLS11
+			log.Println("Minimum TLSv1.1 accepted")
+		}
+	}
+
+	rateLimitedSrv := newRateLimitedServer(srv)
+
+	if *verbose {
+		log.Printf("Listening on %q ...\n", *addr)
+	}
+	log.Fatalln(rateLimitedSrv.ListenAndServe())
+}
+
+type rateLimitedServer struct {
+	*smtpd.Server
+	limiter *rate.Limiter
+}
+
+func (s *rateLimitedServer) ListenAndServe() error {
+	return s.Server.ListenAndServe()
+}
+
+func newRateLimitedServer(srv *smtpd.Server) *rateLimitedServer {
+	return &rateLimitedServer{
+		Server:  srv,
+		limiter: rate.NewLimiter(rate.Every(time.Second), 5),
+	}
+}
+
+func authHandler(_ net.Addr, _ string, username []byte, password []byte, _ []byte) (bool, error) {
+	log.Printf("[AUTH] User: %q; Password: %q\n", username, password)
+	return true, nil
+}
+
+func discardHandler(verbose bool) smtpd.Handler {
+	return func(origin net.Addr, from string, to []string, data []byte) error {
+		if verbose {
+			msg, err := mail.ReadMessage(bytes.NewReader(data))
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+			subject := msg.Header.Get("Subject")
+			log.Printf("Received mail from %q to %q with subject %q (Discarded)\n", from, to, subject)
+		}
+		return nil
+	}
+}
+
+func queuingOutputHandler(queue chan<- *mailDataToSave, verbose bool) smtpd.Handler {
+	return func(origin net.Addr, from string, to []string, data []byte) error {
+		mailToSave := &mailDataToSave{
+			RemoteAddr: origin,
+			From:       from,
+			To:         to,
+			Data:       data,
+			ReceivedAt: time.Now(),
+			RetryCount: 0,
+		}
+
+		select {
+		case queue <- mailToSave:
+			if verbose {
+				msg, err := mail.ReadMessage(bytes.NewReader(data))
+				subject := "no_subject"
+				if err == nil {
+					subject = msg.Header.Get("Subject")
+				}
+				log.Printf("INFO: Mail from %q to %q with subject %q queued for saving.\n", from, to, subject)
+			}
+			return nil
+		default:
+			log.Printf("WARNING: Mail queue is full, could not queue mail from %q to %q. Consider increasing MailQueueBufferSize.\n", from, to)
+			return errors.New("Server queue full, please try again later (internal error)")
+		}
+	}
+}
+
+func saveMailToFile(data *mailDataToSave, ext string, verbose bool) error {
+	if len(data.To) == 0 {
+		return errors.New("no recipient address")
+	}
+
+	toAddress := data.To[0]
+	username := strings.Split(toAddress, "@")[0]
+	if username == "" {
+		return errors.New("could not extract username from address")
+	}
+
+	// Get user info
+	usr, err := user.Lookup(username)
+	if err != nil {
+		return fmt.Errorf("user lookup failed for %q: %v", username, err)
+	}
+
+	// Create mail directory if it doesn't exist
+	mailDir := filepath.Join(usr.HomeDir, "Mail")
+	if err := os.MkdirAll(mailDir, 0700); err != nil {
+		return fmt.Errorf("could not create mail directory: %v", err)
+	}
+
+	// Create inbox if it doesn't exist
+	inboxDir := filepath.Join(mailDir, "inbox")
+	if err := os.MkdirAll(inboxDir, 0700); err != nil {
+		return fmt.Errorf("could not create inbox directory: %v", err)
+	}
+
+	// Generate filename
+	fileName, err := generateRandomFileName(inboxDir, ext)
+	if err != nil {
+		return fmt.Errorf("could not generate filename: %v", err)
+	}
+
+	// Save the file
+	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0600)
+	if err != nil {
+		return fmt.Errorf("could not create file: %v", err)
+	}
+	defer f.Close()
+
+	if _, err = io.Copy(f, bytes.NewReader(data.Data)); err != nil {
+		return fmt.Errorf("could not write to file: %v", err)
+	}
+
+	// Change owner to the target user
+	uid, _ := strconv.Atoi(usr.Uid)
+	gid, _ := strconv.Atoi(usr.Gid)
+	if err := os.Chown(fileName, uid, gid); err != nil {
+		return fmt.Errorf("could not change file owner: %v", err)
+	}
+
+	if verbose {
+		log.Printf("Successfully saved message from %q to %q for user %q\n", data.From, fileName, username)
+	}
+	return nil
+}
+
+func startFileSaverWorkers(queue chan *mailDataToSave, workerCount int, ext string, verbose bool) {
+	for i := 0; i < workerCount; i++ {
+		go func(workerID int) {
+			log.Printf("File Saver Worker %d started.\n", workerID)
+			for mailToSave := range queue {
+				err := saveMailToFile(mailToSave, ext, verbose)
+				if err != nil {
+					log.Printf("File Saver Worker %d: Failed to save mail from %q (Retry %d): %v\n", workerID, mailToSave.From, mailToSave.RetryCount, err)
+					
+					if mailToSave.RetryCount < 3 {
+						mailToSave.RetryCount++
+						go func(m *mailDataToSave) {
+							time.Sleep(5 * time.Second * time.Duration(m.RetryCount))
+							select {
+							case queue <- m:
+								log.Printf("File Saver Worker %d: Re-queued mail from %q for retry %d.\n", workerID, m.From, m.RetryCount)
+							default:
+								log.Printf("File Saver Worker %d: Failed to re-queue mail from %q, queue full. Dropped after %d retries.\n", workerID, m.From, m.RetryCount-1)
+							}
+						}(mailToSave)
+					} else {
+						log.Printf("File Saver Worker %d: Mail from %q permanently failed to save after %d retries.\n", workerID, mailToSave.From, mailToSave.RetryCount)
+					}
+				} else {
+					log.Printf("File Saver Worker %d: Successfully processed mail from %q.\n", workerID, mailToSave.From)
+				}
+			}
+			log.Printf("File Saver Worker %d stopped.\n", workerID)
+		}(i)
+	}
+}
+
+func generateRandomFileName(dir, ext string) (string, error) {
+	const letters = "0123456789abcdefghijklmnopqrstuvwxyz"
+	bytes := make([]byte, RandomNameLength)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", fmt.Errorf("could not generate random filename: %w", err)
+	}
+
+	for i, b := range bytes {
+		bytes[i] = letters[b%byte(len(letters))]
+	}
+
+	fileName := string(bytes) + "." + ext
+	fullPath := filepath.Join(dir, fileName)
+
+	return fullPath, nil
+}
+
+func rcptHandler(_ net.Addr, from string, to string) bool {
+	log.Printf("[RCPT] %q => %q\n", from, to)
+	return true
+}
